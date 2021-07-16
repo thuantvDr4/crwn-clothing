253ba251c356b1006536160412bc08e8a5ee836b
thuantv11 <vinhthuan.tr@gmail.com>
2021-07-16 21:42:56 +0700

    update app with intergration stripe-payment and change app-title and app-icon...

---
+++ src/libs/components/stripe-button/StripeButton.component.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./StripeButton.styles.scss";
+import StripeCheckout from "react-stripe-checkout";
+
+const CODE_TEST = "4242 4242 4242 4242 -----Exp:07/21---CVV:123";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishablekey =
+    "pk_test_51JDrOPBCQQVQ4dX2Kd6qcFPgSvoIYgy3yxDP3GfPd7kL2GzIOA0AgK6M3FF1H1jJZ6smGaaSi6RW9Y0K664vjpbE00Fau2Oc6F";
+
+  //onToken
+  function onToken(token) {
+    console.log("[token]----", token);
+    alert("Payment successful");
+  }
+
+  //----------->
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishablekey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/CheckOut.Page.jsx
+++ src/pages/checkout/CheckOut.Page.jsx
@@ -4,7 +4,9 @@ import { useSelector, useDispatch } from "react-redux";
 import { cartSelector } from "../../redux/selector";
 import CheckoutItem from "../../libs/components/checkout-item/Checkout-Item.Component";
 import { calPriceTotal } from "../../redux/cart/cart.actions";
+import StripeCheckoutButton from "../../libs/components/stripe-button/StripeButton.component";
 
+// ----------->
 const CheckOut = () => {
   //
   const dispatch = useDispatch();
@@ -52,6 +54,14 @@ const CheckOut = () => {
 
       <div className="total-price">
         <span>TOTAL: ${priceTotal}</span>
+      </div>
+      {/* ----payment--- */}
+      <StripeCheckoutButton price={priceTotal} />
+      {/* ----- */}
+      <div className="warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 ------Exp:07/21---CVV:123
       </div>
     </div>
   );
